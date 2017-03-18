746d3cf795f2459be9950a19dcf9f7d9696c4377
SheepOrange <wangjuesheng26@gmail.com>
2017-03-19 04:21:08 +1030

    add 4 pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/about' => 'pages#about'
+  get '/live' => 'pages#live'
+  get '/employment' => 'pages#employment'
+  get '/professional' => 'pages#professional'
+  get '/applications' => 'pages#applications'
+  get '/contact' => 'pages#contact'
   root 'pages#home'
 end
